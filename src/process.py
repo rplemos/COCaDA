c5f1dda34d1fd204a03d0b8f7469d7415b1ce8a0
rplemos <rafael_plemos@hotmail.com>
2025-06-19 19:26:42 -0300

    Added -d parsing for web, moved logic from main to argparser

--- src/process.py
+++ src/process.py
@@ -190,15 +190,6 @@ def process_result(result, context):
             #         f.write(f"{res}\n")  # Writes each residue on a new line
 
 
-def validate_categories(categories):
-    for key, (min_val, max_val) in categories.items():
-        if min_val < 0 or max_val < 0:
-            raise ValueError(f"Invalid values for '{key}': values must be positive.")
-        if min_val >= max_val:
-            raise ValueError(f"Invalid range for '{key}': min ({min_val}) must be less than max ({max_val}).")
-    return categories
-
-
 def log(message, silent=False):
     if not silent:
         print(message)
