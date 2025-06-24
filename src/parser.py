2611232b5423e764913b1a8e5d9180ac4ab3c506
rplemos <rafael_plemos@hotmail.com>
2025-06-24 18:32:25 -0300

    Fix more edge cases on pH values

--- src/parser.py
+++ src/parser.py
@@ -198,7 +198,9 @@ def parse_cif(cif_file):
     models = []
     title = None
     title_block = False
+    
     ph = 7.4
+    experimental_lines = []
     nmr_expt = False
 
     with open(cif_file) as f:
@@ -230,23 +232,28 @@ def parse_cif(cif_file):
                 else:
                     title += line.strip()
                     title_block = False
-
-            if line.startswith("_exptl_crystal_grow.pH"):
+                    
+            # Direct pH tags      
+            if line.startswith("_exptl_crystal_grow.pH ") or line.startswith("_pdbx_nmr_exptl_sample_conditions.pH "):
+                parts = line.split()
+                if len(parts) > 1:
+                    try:
+                        ph = float(parts[1])
+                    except ValueError:
+                        continue
+                    
+            # Handling files where pH value is not directly after the line
+            elif line.startswith("_pdbx_nmr_exptl_sample_conditions"):
+                field = line.split(".")[1]
+                experimental_lines.append(field)
+                nmr_expt = True             
+            elif nmr_expt and line.startswith("1"):
+                parts = line.split()
                 try:
-                    ph = float(line.split()[1])
-                except ValueError:
+                    nmr_ph_index = experimental_lines.index("pH")
+                    ph = float(parts[nmr_ph_index])
+                except (ValueError, IndexError):
                     pass
-                            
-            if line.startswith("_pdbx_nmr_exptl_sample_conditions.pH"):
-                try: 
-                    ph = float(line.split()[1])
-                except ValueError:
-                    pass
-                except IndexError:
-                    nmr_expt = True
-            
-            if nmr_expt and line.startswith("1"):
-                ph = float(line.split()[4])
                 nmr_expt = False
 
             if line.startswith("_atom_site.group_PDB"): # entering ATOM definition block
